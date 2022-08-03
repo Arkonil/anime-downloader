0f1dced92c150ab6e4aceb5edfe4689799a679df
Arkonil <arkonil0904@gmail.com>
2022-08-04 00:25:05 +0530

    added find_packages function

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="anime-downloader",
     version="0.0.1",
-    packages=['anime_downloader'],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'anime-dl=anime_downloader.cli:main'
