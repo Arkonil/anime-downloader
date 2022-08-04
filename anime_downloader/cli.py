97b48756ab23aa8530d78095b81f137fbd198c1f
Arkonil <arkonil0904@gmail.com>
2022-08-04 11:45:01 +0530

    added functionality for cli

--- anime_downloader/cli.py
+++ anime_downloader/cli.py
@@ -1,15 +1,42 @@
 import argparse
+import os
+
+from anime_downloader.sites import AnimixPlay
+
+
+def parse_episode_arg(episodes: str) -> list[int]:
+    try:
+        return [int(episodes)]
+    except ValueError:
+        pass
+
+    if ':' in episodes:
+        pieces = list(map(int, episodes.split(':')))
+        if len(pieces) == 2:
+            return list(range(pieces[0], pieces[1] + 1))
+        elif len(pieces) == 3:
+            return list(range(pieces[0], pieces[1] + 1, pieces[2]))
+
+    elif ',' in episodes:
+        return list(map(int, episodes.split(',')))
+
+    raise ValueError(f"Invalid Argument: {episodes}")
 
 
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('url')
-    # parser.add_argument('episodes', '-e')
+    parser.add_argument('url', help="URL of the anime", type=str)
+    parser.add_argument('episodes', help="Either comma separated episode number or slice")
+    parser.add_argument('--outdir', '-o', help="Output directory (default current directory)", default=os.getcwd())
+    parser.add_argument('--name', '-n', help="Anime Name", default="")
 
     args = parser.parse_args()
-    print(args.url)
 
+    episodes = parse_episode_arg(args.episodes)
+    
+    anime = AnimixPlay(args.url, episodes, args.outdir, args.name)
+    anime.download()
 
 if __name__ == '__main__':
     main()
