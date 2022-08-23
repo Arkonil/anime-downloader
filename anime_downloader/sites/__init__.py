3aa6f5c6cf4f5a8d1b3eba99156d0db394d99675
Arkonil <arkonil0904@gmail.com>
2022-08-23 19:05:04 +0530

    added get_site function

--- anime_downloader/sites/__init__.py
+++ anime_downloader/sites/__init__.py
@@ -1,1 +1,11 @@
+from anime_downloader.sites.BaseSite import BaseSite
 from anime_downloader.sites.AnimixPlay import AnimixPlay
+from anime_downloader.sites.Zoro import Zoro
+
+def get_site(url):
+    if 'animixplay' in url:
+        return AnimixPlay
+    elif 'zoro' in url:
+        return Zoro
+    else:
+        raise ValueError(f"Unknown site url provided: {url}")
